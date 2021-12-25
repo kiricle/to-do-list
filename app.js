003614ae6f5873b07bd541d008205f6d8b9bc514
Kolotylo Kyrylo <kolot102@gmail.com>
2021-12-25 16:01:48 +0200

    add todays list to fill in using ejs

--- app.js
+++ app.js
@@ -2,3 +2,35 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+let items = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  const today = new Date();
+
+  const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+  };
+
+  const currentDay = today.toLocaleDateString('en-US', options);
+
+  res.render('list', { kindOfDay: currentDay, newListItems: items });
+});
+
+app.post('/', (req, res) => {
+  const { newItem } = req.body;
+
+  items.push(newItem);
+
+  res.redirect('/');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
